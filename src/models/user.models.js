a7d4deb7df2aea139cc547045556409c8beb4b71
Shripad Dhanshetti <shripad1483@gmail.com>
2026-02-02 20:19:34 +0530

    feat(user.models.js): Write password hashing logic using mongoose pre hook.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -59,6 +59,12 @@ const userSchema = new Schema(
     }
 );
 
+// Hash password in DB before saving it. 
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
 
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
 export const User = mongoose.model("User", userSchema);
